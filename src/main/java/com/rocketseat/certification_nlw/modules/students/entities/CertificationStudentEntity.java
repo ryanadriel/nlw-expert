6ef5a27f17424a31b557064501eb602f72ae8570
Adriel Ryan <adrielryan.tj@hotmail.com>
2024-02-08 10:31:19 -0300

    Atribuindo colunas para studentEntitiy e certificationStudentEntity

--- src/main/java/com/rocketseat/certification_nlw/modules/students/entities/CertificationStudentEntity.java
+++ src/main/java/com/rocketseat/certification_nlw/modules/students/entities/CertificationStudentEntity.java
@@ -1,8 +1,15 @@
 package com.rocketseat.certification_nlw.modules.students.entities;
 
-import java.util.List;
+//import java.util.List;
 import java.util.UUID;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,11 +17,25 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity(name = "certifications")
 public class CertificationStudentEntity {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
-    private UUID studanteID;
+
+    @Column(length = 100)
     private String technology;
+
+    @Column(length = 10)
     private int grade;
-    List<AnswerCertificationsEntity> answerCertificationsEntities;
+    
+    @JoinColumn(name = "student_id")
+    private UUID studentID;
+
+    @ManyToOne
+    @JoinColumn(name = "student_id", insertable = false, updatable = false)
+    private StudentEntity studentEntity;
+
+    //List<AnswerCertificationsEntity> answerCertificationsEntities;
 }
